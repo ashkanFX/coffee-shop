a1dbe233d4cf14b400584833b3e192aae492d0c1
Ashkan Fotohi <ashkanfotohi81@gmial.com>
2022-08-24 08:28:29 +0430

    navBar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ButtonModule} from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminProductComponent } from './admin-product/admin-product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AdminProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
