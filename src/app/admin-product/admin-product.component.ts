bf26683aadc3f364cda7e4589c6b912c8751eeed
Ashkan Fotohi <ashkanfotohi81@gmial.com>
2022-08-26 14:18:20 +0430

    make program directive

--- src/app/admin-product/admin-product.component.ts
+++ src/app/admin-product/admin-product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+ 
+interface menu {
+  image : string,
+  name : string
+}
 
 @Component({
   selector: 'app-admin-product',
@@ -8,6 +13,25 @@ import { Component, OnInit } from '@angular/core';
 export class AdminProductComponent implements OnInit {
 
   constructor() { }
+
+  menus : menu[] =[
+    {
+      image : '../../assets/img/pexels-mateusz-dach-362137.jpg',
+      name : 'قهو ها'
+    },
+    {
+      image : '../../assets/img/pexels-olena-bohovyk-1772124.jpg',
+      name : 'دمنوش ها'
+    },
+    {
+      image : '../../assets/img/pexels-valeria-boltneva-827516.jpg',
+      name : 'کیک ها'
+    },
+    {
+      image : '../../assets/img/pexels-alisha-mishra-1346341.jpg',
+      name : ' بستنی ها'
+    }
+  ]
 
   ngOnInit(): void {
   }
