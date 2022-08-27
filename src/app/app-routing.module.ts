924decab0acb10b122c8b150be446c837d171ce6
Ashkan Fotohi <ashkanfotohi81@gmial.com>
2022-08-27 15:33:12 +0430

    add route and add product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminProductComponent } from './admin-product/admin-product.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'',redirectTo:'Home',pathMatch:'full'},
+  {path :'Home',component:AdminProductComponent},
+  {path:'Home/product',component:ProductComponent},
+  {path:'**',redirectTo:'Home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AdminProductComponent } from './admin-product/admin-product.component';
 import { FooterComponent } from './footer/footer.component';
 import {ImageModule} from 'primeng/image';
+import { ProductComponent } from './product/product.component';
+import {BadgeModule} from 'primeng/badge';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AdminProductComponent,
-    FooterComponent
+    FooterComponent,
+    ProductComponent
   ],
   imports: [
+    BadgeModule,
     BrowserModule,
     ImageModule,
+    InputNumberModule,
     ButtonModule,
+    RadioButtonModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
